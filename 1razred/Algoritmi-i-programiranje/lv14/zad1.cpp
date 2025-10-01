2b78be515300aa4f022b8f255b4fb5789a314447
Black_Knight <155252713+Black-Knight4@users.noreply.github.com>
2025-10-01 21:43:16 +0200

    Add files via upload

---
+++ 1razred/Algoritmi-i-programiranje/lv14/zad1.cpp
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+int main(void) {
+
+	int oc1, oc2, oc3;
+	float ar;
+
+	printf("Upisi 3 ocjene: ");
+	scanf_s("%d %d %d", &oc1, &oc2, &oc3);
+
+	if (oc1 > 5 || oc1 < 1 || oc2 > 5 || oc2 < 1 || oc3 > 5 || oc3 < 1) {
+
+		printf("Nisu ispravno upisane ocjene!");
+
+	}
+	else {
+
+		ar = (oc1 + oc2 + oc3) / 3.0;
+		printf("Aritmeticka sredina upisanih brojeva %d, %d i %d je %.2f", oc1, oc2, oc3, ar);
+
+	}
+	return 0;
+
+}

1d8de3eb8166f2c51cd7444639386982161ba6a7
Black_Knight <155252713+Black-Knight4@users.noreply.github.com>
2025-10-01 22:42:50 +0200

    Add files via upload

---
+++ 1razred/Algoritmi-i-programiranje/lv14/zad2.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <math.h>
+int main(void) {
+
+	float br;
+
+	printf("Upisi neki broj: ");
+	scanf_s("%f", &br);
+
+	printf("Drugi korijen iz: \n");
+	printf("-cjelog dijela broja %.2f: %.2f\n", br, sqrt((int)br));
+	printf("-decimalnog dijela broja %.2f: %.2f", br, sqrt(br - (int)br));
+
+}

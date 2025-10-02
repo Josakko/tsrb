91cbdbc9cb78d0401904fa74ac75c27faf12d8cc
Josakko <112590914+Josakko@users.noreply.github.com>
2025-10-02 02:28:47 +0200

    beautified a bit (yikes)

--- 1razred/Algoritmi-i-programiranje/lv14/zad1.c
+++ 1razred/Algoritmi-i-programiranje/lv14/zad1.c
@@ -1,7 +1,6 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main(void) {
-
 	int oc1, oc2, oc3;
 	float ar;
 
@@ -9,16 +8,10 @@ int main(void) {
 	scanf_s("%d %d %d", &oc1, &oc2, &oc3);
 
 	if (oc1 > 5 || oc1 < 1 || oc2 > 5 || oc2 < 1 || oc3 > 5 || oc3 < 1) {
-
 		printf("Nisu ispravno upisane ocjene!");
-
-	}
-	else {
-
+	} else {
 		ar = (oc1 + oc2 + oc3) / 3.0;
 		printf("Aritmeticka sredina upisanih brojeva %d, %d i %d je %.2f", oc1, oc2, oc3, ar);
-
 	}
-	return 0;
-
+	
 }
